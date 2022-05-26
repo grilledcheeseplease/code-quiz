9098a319b68d1ea4cb03178e417e9fc95e3b3cf0
grilledcheeseplease <lacynunnally402@gmail.com>
2022-05-26 01:44:46 -0400

    I'm sleepy and this is hard, added comments and funtions I think I may need

--- script.js
+++ script.js
@@ -18,74 +18,75 @@ function displayTime() {
 
 
 function setTime() {
-
     displayTime();
-
+    // TODO: have questions start 
     var timerInterval = setInterval(function () {
         timeLeft--;
         displayTime();
-        
+
         if (timeLeft < 1) {
             clearInterval(timerInterval);
-            //add action for gameover content 
+            // TODO: add action for gameover content 
         }
 
     }, 1000);
-    
+
 }
 
-var qAndA =[
+var qAndA = [
     {
-        question : "Around how many coding languages are there?", 
+        question: "Around how many coding languages are there?",
         answers: {
             1: "50",
             2: "600",
             3: "450",
             4: "700"
         },
-        correctAnswer : "4"
+        correctAnswer: "4"
     },
     {
-        question : "The world's first computer programmer was a renowned female mathematician. What is her name?",
+        question: "The world's first computer programmer was a renowned female mathematician. What is her name?",
         answers: {
             1: "Ada Lovelace",
             2: "Mary Barnes",
             3: "Joanna Fields",
             4: "Karen Mays"
         },
-        correctAnswer : "1"
+        correctAnswer: "1"
     },
     {
-        question : "What war was computer programming instumental in helping end?",
+        question: "What war was computer programming instumental in helping end?",
         answers: {
             1: "Cold war",
             2: "World War II",
             3: "World War I",
             4: "The Mexican-American War"
         },
-        correctAnswer : "2"
+        correctAnswer: "2"
     },
     {
-        question : "The first computer virus was created in 1986 and was given what moniker?",
+        question: "The first computer virus was created in 1986 and was given what moniker?",
         answers: {
             1: "Scorpion",
             2: "Alien",
             3: "Brain",
             4: "Pork"
         },
-        correctAnswer : "3"
+        correctAnswer: "3"
     },
     {
-        question : "How many times did I cry while creating this?",
+        question: "How many times did I cry while creating this?",
         answers: {
             1: "Big girls don't cry.",
             2: "I lost count",
             3: "Only one time!",
             4: "Probably still crying."
         },
-        correctAnswer : "3"
+        correctAnswer: "3"
     }
 ];
+
+
 // event listeners here
 startEl.addEventListener("click", setTime);
 
